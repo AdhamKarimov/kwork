ac147e3dcc89e4ddeb2f31f3a60414672a841253
ADHAM <adhamkarimov0000@gmail.com>
2026-03-23 21:23:53 +0500

    01

--- notifications/urls.py
+++ notifications/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 app_name = 'notifications'
 
-
 urlpatterns = [
-    path('', views.NotificationListView.as_view(), name='list'),
-    path('mark-read/<int:pk>/', views.MarkAsReadView.as_view(), name='mark_read'),
+    path('',                views.NotificationListView.as_view(), name='list'),
+    path('<int:pk>/read/',  views.MarkAsReadView.as_view(),        name='mark_read'),
+    path('read-all/',       views.MarkAllReadView.as_view(),       name='mark_all_read'),
+    path('unread-count/',   views.UnreadCountView.as_view(),       name='unread_count'),
 ]
